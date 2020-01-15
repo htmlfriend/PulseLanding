8906f9bf6aede00825c3e9134779e02eff7599e4
Yurii <yuriy.tim@yahoo.com>
2020-01-14 23:45:41 -0500

    deploy2

--- dist/js/script.js
+++ dist/js/script.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
     $(".overlay, #consultation").fadeIn("slow");
   });
   $(".modal__close").on("click", function() {
-    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+    $(".overlay, #consultation,#thanks,#order").fadeOut("slow");
   });
 
   $(".button_mini").each(function(i) {
@@ -102,7 +102,7 @@ $(document).ready(function() {
         .find("input")
         .val("");
       $("#consultation, #order").fadeOut();
-      $(".overlay, #thanks").fadeIn();
+      $(".overlay, #thanks").fadeIn("slow");
       $("form").trigger("reset");
     });
     return false;
